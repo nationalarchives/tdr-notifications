bc1c9a656503d8bce2206b7e02a62c683e913562
sam <sam.palmer@nationalarchives.gov.uk>
2020-10-22 14:12:07 +0100

    Check to see if scan notification should be sent.
    Add the image tags to the decoder and then check to see if the messages
    should be sent. If all scan results are zero or the image tag isn't in
    the allowed list, it won't send.

--- src/main/scala/uk/gov/nationalarchives/scannotifications/messages/EventMessages.scala
+++ src/main/scala/uk/gov/nationalarchives/scannotifications/messages/EventMessages.scala
@@ -8,7 +8,7 @@ import cats.syntax.option._
 import uk.gov.nationalarchives.aws.utils.SESUtils
 import uk.gov.nationalarchives.aws.utils.SESUtils.Email
 import uk.gov.nationalarchives.scannotifications.decoders.SSMMaintenanceDecoder.SSMMaintenanceEvent
-import uk.gov.nationalarchives.scannotifications.decoders.ScanDecoder.ScanEvent
+import uk.gov.nationalarchives.scannotifications.decoders.ScanDecoder.{ScanDetail, ScanEvent}
 
 object EventMessages {
 
@@ -19,41 +19,55 @@ object EventMessages {
   case class SlackMessage(blocks: List[SlackBlock])
 
   implicit val scanEventMessages: Messages[ScanEvent] = new Messages[ScanEvent] {
+    private val allowedTags = Set("latest", "intg", "staging", "prod")
+
     private def slackBlock(text: String) = SlackBlock("section", SlackText("mrkdwn", text))
 
     private def countBlock(count: Option[Int], level: String) = slackBlock(s"${count.getOrElse(0)} $level severity vulnerabilities")
 
+    private def allowedTagMatches(detail: ScanDetail): Set[String] = detail.tags.toSet.intersect(allowedTags)
+
+    private def shouldSendNotification(detail: ScanDetail): Boolean = allowedTagMatches(detail).nonEmpty && !detail.findingSeverityCounts.areAllZero()
+
     override def slack(event: ScanEvent) = {
       val detail = event.detail
-      val headerBlock = slackBlock(s"*ECR image scan complete on image ${detail.repositoryName}*")
-      val severityCounts = detail.findingSeverityCounts
-      val criticalBlock = countBlock(severityCounts.critical, "critical")
-      val highBlock = countBlock(severityCounts.high, "high")
-      val mediumBlock = countBlock(severityCounts.medium, "medium")
-      val lowBlock = countBlock(severityCounts.low, "low")
-      SlackMessage(List(headerBlock, criticalBlock, highBlock, mediumBlock, lowBlock))
-        .asJson.noSpaces.some
+      if(shouldSendNotification(detail)) {
+        val headerBlock = slackBlock(s"*ECR image scan complete on image ${detail.repositoryName} ${detail.tags.mkString(",")}*")
+        val severityCounts = detail.findingSeverityCounts
+        val criticalBlock = countBlock(severityCounts.critical, "critical")
+        val highBlock = countBlock(severityCounts.high, "high")
+        val mediumBlock = countBlock(severityCounts.medium, "medium")
+        val lowBlock = countBlock(severityCounts.low, "low")
+        SlackMessage(List(headerBlock, criticalBlock, highBlock, mediumBlock, lowBlock))
+          .asJson.noSpaces.some
+      } else {
+        Option.empty
+      }
     }
 
     override def email(event: ScanEvent): Option[SESUtils.Email] = {
       val detail = event.detail
-      val critical = detail.findingSeverityCounts.critical.getOrElse(0)
-      val high = detail.findingSeverityCounts.high.getOrElse(0)
-      val medium = detail.findingSeverityCounts.medium.getOrElse(0)
-      val low = detail.findingSeverityCounts.low.getOrElse(0)
+      if(shouldSendNotification(detail)) {
+        val critical = detail.findingSeverityCounts.critical.getOrElse(0)
+        val high = detail.findingSeverityCounts.high.getOrElse(0)
+        val medium = detail.findingSeverityCounts.medium.getOrElse(0)
+        val low = detail.findingSeverityCounts.low.getOrElse(0)
 
-      val message = html(
-        body(
-          h1(s"Image scan results for ${detail.repositoryName}"),
-          div(
-            p(s"$critical critical vulnerabilities"),
-            p(s"$high high vulnerabilities"),
-            p(s"$medium medium vulnerabilities"),
-            p(s"$low low vulnerabilities")
+        val message = html(
+          body(
+            h1(s"Image scan results for ${detail.repositoryName}"),
+            div(
+              p(s"$critical critical vulnerabilities"),
+              p(s"$high high vulnerabilities"),
+              p(s"$medium medium vulnerabilities"),
+              p(s"$low low vulnerabilities")
+            )
           )
-        )
-      ).toString()
-      Email("scanresults@tdr-management.nationalarchives.gov.uk", ConfigFactory.load.getString("ses.email.to"), s"ECR scan results for ${detail.repositoryName}", message).some
+        ).toString()
+        Email("scanresults@tdr-management.nationalarchives.gov.uk", ConfigFactory.load.getString("ses.email.to"), s"ECR scan results for ${detail.repositoryName}", message).some
+      } else {
+        Option.empty
+      }
     }
   }
 
