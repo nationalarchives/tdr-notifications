c0340558a2511c2fe7a098c957955c86dbad8513
MancunianSam <sam.palmer@nationalarchives.gov.uk>
2021-03-25 12:14:13 +0000

    Return kms response based on the request

--- src/test/scala/uk/gov/nationalarchives/notifications/LambdaSpecUtils.scala
+++ src/test/scala/uk/gov/nationalarchives/notifications/LambdaSpecUtils.scala
@@ -1,23 +1,44 @@
 package uk.gov.nationalarchives.notifications
 
 import com.github.tomakehurst.wiremock.WireMockServer
+import com.github.tomakehurst.wiremock.client.ResponseDefinitionBuilder
 import com.github.tomakehurst.wiremock.client.WireMock.{equalToJson, ok, okJson, post, urlEqualTo}
+import com.github.tomakehurst.wiremock.common.FileSource
+import com.github.tomakehurst.wiremock.core.WireMockConfiguration
+import com.github.tomakehurst.wiremock.extension.{Parameters, ResponseDefinitionTransformer}
+import com.github.tomakehurst.wiremock.http.{Request, ResponseDefinition}
 import com.github.tomakehurst.wiremock.stubbing.StubMapping
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 import org.scalatest.{BeforeAndAfterAll, BeforeAndAfterEach}
+import io.circe.generic.auto._
+import io.circe.parser.decode
+
+import java.nio.ByteBuffer
+import java.nio.charset.Charset
 
 class LambdaSpecUtils extends AnyFlatSpec with Matchers with BeforeAndAfterAll with BeforeAndAfterEach {
 
   val wiremockSesEndpoint = new WireMockServer(9001)
   val wiremockSlackServer = new WireMockServer(9002)
-  val wiremockKmsEndpoint = new WireMockServer(9004)
+  val wiremockKmsEndpoint = new WireMockServer(new WireMockConfiguration().port(9004).extensions(new ResponseDefinitionTransformer {
+    override def transform(request: Request, responseDefinition: ResponseDefinition, files: FileSource, parameters: Parameters): ResponseDefinition = {
+      case class KMSRequest(CiphertextBlob: String)
+      decode[KMSRequest](request.getBodyAsString) match {
+        case Left(err) => throw err
+        case Right(req) =>
+          val charset = Charset.defaultCharset()
+          val plainText = charset.newDecoder.decode(ByteBuffer.wrap(req.CiphertextBlob.getBytes(charset))).toString
+          ResponseDefinitionBuilder
+            .like(responseDefinition)
+            .withBody(s"""{"Plaintext": "$plainText"}""")
+            .build()
+      }
+    }
+    override def getName: String = ""
+  }))
 
-  def stubKmsResponse(cipherText: String): StubMapping =
-    wiremockKmsEndpoint.stubFor(post(urlEqualTo("/"))
-      .withRequestBody(equalToJson(s"""{"CiphertextBlob":"$cipherText","EncryptionContext":{"LambdaFunctionName":"test-lambda-function"}}"""))
-      .willReturn(okJson(s"""{"Plaintext": "$cipherText"}"""))
-    )
+  def stubKmsResponse: StubMapping = wiremockKmsEndpoint.stubFor(post(urlEqualTo("/")))
 
   override def beforeEach(): Unit = {
     wiremockSlackServer.stubFor(post(urlEqualTo("/webhook")).willReturn(ok("")))
@@ -34,9 +55,7 @@ class LambdaSpecUtils extends AnyFlatSpec with Matchers with BeforeAndAfterAll w
           |</SendEmailResponse>
           |""".stripMargin)))
 
-    stubKmsResponse("aHR0cDovL2xvY2FsaG9zdDo5MDAyL3dlYmhvb2s=")
-    stubKmsResponse("YXdzX3Rkcl9tYW5hZ2VtZW50QG5hdGlvbmFsYXJjaGl2ZXMuZ292LnVr")
-    stubKmsResponse("Q1ZFLTIwMTYtMTIzNDU2NzgsQ1ZFLTIwMjAtOTg3NjU=")
+    stubKmsResponse
 
 
     super.beforeEach()
