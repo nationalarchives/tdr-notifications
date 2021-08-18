1688c96c8e57fffe4f9378c77bd6ff903f517564
TomJKing <tom.king@nationalarchives.gov.uk>
2021-08-18 10:35:50 +0100

    Remove duplicated code for SNS messages

--- src/main/scala/uk/gov/nationalarchives/notifications/decoders/KeycloakEventDecoder.scala
+++ src/main/scala/uk/gov/nationalarchives/notifications/decoders/KeycloakEventDecoder.scala
@@ -1,19 +1,15 @@
 package uk.gov.nationalarchives.notifications.decoders
 
-import io.circe.CursorOp.DownField
 import io.circe.generic.auto._
-import io.circe.parser.parse
-import io.circe.{Decoder, DecodingFailure, HCursor}
+import io.circe.{Decoder, HCursor}
+import uk.gov.nationalarchives.notifications.decoders.IncomingEvent.{Record, parseSNSMessage}
 
 object KeycloakEventDecoder {
   case class KeycloakEvent(tdrEnv: String, message: String) extends IncomingEvent
-  case class SNS(Message: String)
-  case class Record(Sns: SNS)
 
   val decodeKeycloakEvent: Decoder[IncomingEvent] = (c: HCursor) => for {
     messages <- c.downField("Records").as[List[Record]]
-    json <- parse(messages.head.Sns.Message)
-      .left.map(e => DecodingFailure.fromThrowable(e, List(DownField("Message"))))
+    json <- parseSNSMessage(messages.head.Sns.Message)
     event <- json.as[KeycloakEvent]
   } yield event
 }
