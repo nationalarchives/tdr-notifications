b2ae2e2b72ae366eddf45b2d4ad8b6efe06e3c27
MancunianSam <sam.palmer@nationalarchives.gov.uk>
2021-09-02 12:44:20 +0100

    Abstract out the decode method.
    The three methods I've replaced here all did the same thing apart from
    they decoded to a different case class at the end so I've added in a
    generic method in the IncomingEvent class which is now used instead.

--- src/main/scala/uk/gov/nationalarchives/notifications/decoders/ExportStatusDecoder.scala
+++ src/main/scala/uk/gov/nationalarchives/notifications/decoders/ExportStatusDecoder.scala
@@ -2,10 +2,6 @@ package uk.gov.nationalarchives.notifications.decoders
 
 import java.util.UUID
 
-import io.circe.generic.auto._
-import io.circe.{Decoder, HCursor}
-import uk.gov.nationalarchives.notifications.decoders.IncomingEvent.{Record, parseSNSMessage}
-
 object ExportStatusDecoder {
   case class ExportSuccessDetails(userId: UUID, consignmentReference: String, transferringBodyCode: String)
   case class ExportStatusEvent(
@@ -14,9 +10,4 @@ object ExportStatusDecoder {
                                 environment: String,
                                 successDetails: Option[ExportSuccessDetails],
                                 failureCause: Option[String]) extends IncomingEvent
-  val decodeExportStatusEvent: Decoder[IncomingEvent] = (c: HCursor) => for {
-    messages <- c.downField("Records").as[List[Record]]
-    json <- parseSNSMessage(messages.head.Sns.Message)
-    status <- json.as[ExportStatusEvent]
-  } yield status
 }
