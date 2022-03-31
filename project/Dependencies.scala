24a5ea4223a1c795fc4e4243545d365a76ba6672
tna-digital-archiving-jenkins <digitalpreservation@nationalarchives.gov.uk>
2022-03-31 08:34:04 +0000

    Update async-http-client-backend-cats, ... to 3.3.18

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,7 +2,7 @@ import sbt._
 
 object Dependencies {
   private val circeVersion = "0.13.0"
-  private val sttpClient3Version = "3.3.15"
+  private val sttpClient3Version = "3.3.18"
   private val elasticMqVersion = "1.1.1"
 
   lazy val sttp = "com.softwaremill.sttp.client3" %% "async-http-client-backend-cats" % sttpClient3Version
