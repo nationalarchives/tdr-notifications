fee9f9e971183db20c18ce9a19bb5f1eeb755669
MancunianSam <sam.palmer@nationalarchives.gov.uk>
2022-08-08 08:50:57 +0100

    Code review changes

--- src/test/scala/uk/gov/nationalarchives/notifications/GenericMessageIntegrationSpec.scala
+++ src/test/scala/uk/gov/nationalarchives/notifications/GenericMessageIntegrationSpec.scala
@@ -5,25 +5,24 @@ import org.scalatest.prop.TableFor7
 
 class GenericMessageIntegrationSpec extends LambdaIntegrationSpec {
 
-
   override def events: TableFor7[String, String, Option[String], Option[String], Option[SqsExpectedMessageDetails], () => Unit, String] = {
     val oneMessage: String =
       """ "{\"messages\":[{\"message\":\"A test message\"}]}" """
-    val successfulMessageText = "A test message"
+    val oneMessageText = "A test message"
     val twoMessages: String =
       """ "{\"messages\":[{\"message\":\"A test message\"}, {\"message\":\"A second test message\"}]}" """
-    val failedMessageText = "A test message\\nA second test message"
+    val twoMessagesText = "A test message\\nA second test message"
 
     Table(
       ("description", "input", "emailBody", "slackBody", "sqsMessage", "stubContext", "slackUrl"),
-      ("a single message",
-        secretRotationEventInput(oneMessage), None, slackMsg(successfulMessageText).some, None, () => (), "/webhook"),
-      ("multiple messages",
-        secretRotationEventInput(twoMessages), None, slackMsg(failedMessageText).some, None, () => (), "/webhook")
+      ("one message",
+        genericEventInput(oneMessage), None, slackMsg(oneMessageText).some, None, () => (), "/webhook"),
+      ("two messages",
+        genericEventInput(twoMessages), None, slackMsg(twoMessagesText).some, None, () => (), "/webhook")
     )
   }
 
-  def secretRotationEventInput(rotationNotification: String): String =
+  def genericEventInput(rotationNotification: String): String =
     s"""{
        | "Records": [
        |   {
