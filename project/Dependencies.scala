acb76e6584a70fb0f5d63c1e2c2ccce7791b3f7b
tna-digital-archiving-jenkins <digitalpreservation@nationalarchives.gov.uk>
2023-11-10 08:31:57 +0000

    Update async-http-client-backend-cats, ... to 3.9.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,7 +2,7 @@ import sbt._
 
 object Dependencies {
   private val circeVersion = "0.14.6"
-  private val sttpClient3Version = "3.9.0"
+  private val sttpClient3Version = "3.9.1"
   private val elasticMqVersion = "1.5.0"
   private val awsUtilsVersion = "0.1.112"
 
