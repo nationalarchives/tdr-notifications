9f687dee6bfe7f0b18d0905441fbc4c81d874d1c
tna-digital-archiving-jenkins <digitalpreservation@nationalarchives.gov.uk>
2024-06-16 08:27:39 +0000

    Update ecr-utils, kms-utils, ses-utils, ... to 0.1.186 in scala-steward-dependencies

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,7 +4,7 @@ object Dependencies {
   private val circeVersion = "0.14.7"
   private val sttpClient3Version = "3.9.7"
   private val elasticMqVersion = "1.6.3"
-  private val awsUtilsVersion = "0.1.185"
+  private val awsUtilsVersion = "0.1.186"
 
   lazy val sttp = "com.softwaremill.sttp.client3" %% "async-http-client-backend-cats" % sttpClient3Version
   lazy val sttpCirce = "com.softwaremill.sttp.client3" %% "circe" % sttpClient3Version
