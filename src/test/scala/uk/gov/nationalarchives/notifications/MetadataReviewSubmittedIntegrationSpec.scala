6b0d4ccfed188a6b6ae6581b846ac2088e7336fd
Thanh <thanh.lam@nationalarchives.gov.uk>
2024-08-01 13:13:32 +0100

    *MetadataReviewSubmitted Unit test

--- src/test/scala/uk/gov/nationalarchives/notifications/MetadataReviewSubmittedIntegrationSpec.scala
+++ src/test/scala/uk/gov/nationalarchives/notifications/MetadataReviewSubmittedIntegrationSpec.scala
@@ -6,7 +6,7 @@ import uk.gov.nationalarchives.notifications.messages.EventMessages.GovUKEmailDe
 class MetadataReviewSubmittedIntegrationSpec extends LambdaIntegrationSpec {
   override lazy val events: Seq[Event] = Seq(
     Event(
-      description = "A metadata review submitted event",
+      description = "An approved metadata review submitted event",
       input = metadataReviewSubmittedNotificationInputString(
         MetadataReviewSubmittedEvent(
           consignmentReference = "SomeConsignmentReference",
@@ -20,7 +20,32 @@ class MetadataReviewSubmittedIntegrationSpec extends LambdaIntegrationSpec {
         govUKEmail = Some(
           GovUKEmailDetails(
             reference = "SomeConsignmentReference",
-            templateId = "TestTemplateId",
+            templateId = "TestApprovedTemplateId",
+            userEmail = "email@mail.com",
+            personalisation = Map(
+              "urlLink" -> "example.com",
+              "consignmentReference" -> "SomeConsignmentReference",
+            )
+          )
+        )
+      )
+    ),
+    Event(
+      description = "A rejected metadata review submitted event",
+      input = metadataReviewSubmittedNotificationInputString(
+        MetadataReviewSubmittedEvent(
+          consignmentReference = "SomeConsignmentReference",
+          urlLink = "example.com",
+          userEmail = "email@mail.com",
+          status = "CompletedWithIssues"
+        )
+      ),
+      stubContext = stubDummyGovUkNotifyEmailResponse,
+      expectedOutput = ExpectedOutput(
+        govUKEmail = Some(
+          GovUKEmailDetails(
+            reference = "SomeConsignmentReference",
+            templateId = "TestRejectedTemplateId",
             userEmail = "email@mail.com",
             personalisation = Map(
               "urlLink" -> "example.com",
