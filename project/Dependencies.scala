4ee4d53ec6f2ca6359b6d624d90282f9b7424037
tna-digital-archiving-jenkins <digitalperservation@nationalarchives.gov.uk>
2024-08-19 13:09:58 +0000

    Update async-http-client-backend-cats, ... to 3.9.8 in scala-steward-dependencies

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,7 +2,7 @@ import sbt._
 
 object Dependencies {
   private val circeVersion = "0.14.9"
-  private val sttpClient3Version = "3.9.7"
+  private val sttpClient3Version = "3.9.8"
   private val awsUtilsVersion = "0.1.201"
   private val elasticMqVersion = "1.6.6"
   lazy val sttp = "com.softwaremill.sttp.client3" %% "async-http-client-backend-cats" % sttpClient3Version
