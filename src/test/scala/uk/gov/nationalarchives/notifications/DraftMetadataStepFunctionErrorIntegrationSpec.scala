daf1b1c8ffdffdc03efeca2abcd75d40cd4d8d87
ian-hoyle <ian.hoyle@nationalarchives.gov.uk>
2024-11-12 12:19:30 +0000

    Error Message will be wrapped (#427)
    The metadata error sent to SNS will be wrapped for the Lambda

--- src/test/scala/uk/gov/nationalarchives/notifications/DraftMetadataStepFunctionErrorIntegrationSpec.scala
+++ src/test/scala/uk/gov/nationalarchives/notifications/DraftMetadataStepFunctionErrorIntegrationSpec.scala
@@ -4,6 +4,7 @@ import io.circe.Printer
 import io.circe.generic.auto._
 import io.circe.syntax._
 import uk.gov.nationalarchives.notifications.decoders.DraftMetadataStepFunctionErrorDecoder.DraftMetadataStepFunctionError
+import uk.gov.nationalarchives.notifications.decoders.IncomingEvent.{SNS, SnsRecord}
 
 import java.util.UUID
 
@@ -11,17 +12,17 @@ class DraftMetadataStepFunctionErrorIntegrationSpec extends LambdaIntegrationSpe
   override lazy val events: Seq[Event] = Seq(
     Event(
       description = "a draft metadata step function error on intg",
-      input = draftMetaDataStepFunctionErrorInput(draftMetadataStepFunctionError("intg")),
+      input = draftMetaDataStepFunctionErrorSnsInput(draftMetadataStepFunctionError("intg")),
       expectedOutput = ExpectedOutput()
     ),
     Event(
       description = "a draft metadata step function error on staging",
-      input = draftMetaDataStepFunctionErrorInput(draftMetadataStepFunctionError("staging")),
+      input = draftMetaDataStepFunctionErrorSnsInput(draftMetadataStepFunctionError("staging")),
       expectedOutput = ExpectedOutput()
     ),
     Event(
       description = "a draft metadata step function error on prod",
-      input = draftMetaDataStepFunctionErrorInput(draftMetadataStepFunctionError("prod")),
+      input = draftMetaDataStepFunctionErrorSnsInput(draftMetadataStepFunctionError("prod")),
       expectedOutput = ExpectedOutput(
         slackMessage = Some(SlackMessage(slackMessage(draftMetadataStepFunctionError("prod")).get, "/webhook-tdr"))
       )
@@ -34,8 +35,10 @@ class DraftMetadataStepFunctionErrorIntegrationSpec extends LambdaIntegrationSpe
     DraftMetadataStepFunctionError(id, "error", "cause", environment)
   }
 
-  def draftMetaDataStepFunctionErrorInput(draftMetadataStepFunctionError: DraftMetadataStepFunctionError): String = {
-    draftMetadataStepFunctionError.asJson.printWith(Printer.noSpaces)
+  def draftMetaDataStepFunctionErrorSnsInput(draftMetadataStepFunctionError: DraftMetadataStepFunctionError): String = {
+    case class Records(Records:List[SnsRecord])
+    val snsEvent = Records(List(SnsRecord(SNS(draftMetadataStepFunctionError.asJson.toString()))))
+    snsEvent.asJson.printWith(Printer.noSpaces)
   }
 
   def slackMessage(draftMetadataStepFunctionError: DraftMetadataStepFunctionError): Option[String] = Option {
