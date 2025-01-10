9d54bcdeac03397eac9ce04692adeeedd12ca2fb
Tom Hallett <tehallett+github@gmail.com>
2025-01-10 11:59:48 +0000

    Fixing unit tests

--- src/test/scala/uk/gov/nationalarchives/notifications/KeycloakEventIntegrationSpec.scala
+++ src/test/scala/uk/gov/nationalarchives/notifications/KeycloakEventIntegrationSpec.scala
@@ -10,6 +10,13 @@ class KeycloakEventIntegrationSpec extends LambdaIntegrationSpec {
       description = "a keycloak event message",
       input = scanEventInputText(keycloakEvent),
       expectedOutput = ExpectedOutput(
+        slackMessage = Some(SlackMessage(body = expectedKeycloakEventSlackMessage, webhookUrl = "/webhook-bau"))
+      )
+    ),
+    Event(
+      description = "a keycloak event message on prod",
+      input = scanEventInputText(keycloakEvent.copy(tdrEnv = "prod")),
+      expectedOutput = ExpectedOutput(
         slackMessage = Some(SlackMessage(body = expectedKeycloakEventSlackMessage, webhookUrl = "/webhook-tdr"))
       )
     ),
