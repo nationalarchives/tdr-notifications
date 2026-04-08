ddf24690267e3ab594f0b3c07fdc1d598335908e
Annie Hawes <annie.hawes@nationalarchives.gov.uk>
2026-04-08 16:37:27 +0100

    Testing change

--- src/main/scala/uk/gov/nationalarchives/notifications/messages/EventMessages.scala
+++ src/main/scala/uk/gov/nationalarchives/notifications/messages/EventMessages.scala
@@ -166,7 +166,7 @@ object EventMessages {
     }
 
     override def slack(incomingEvent: ExportStatusEvent, context: Unit): Option[SlackMessage] = {
-      if (!incomingEvent.success) {
+      if (true/*incomingEvent.environment != "intg" || !incomingEvent.success*/) {
 
         val exportInfoMessage = constructExportInfoMessage(incomingEvent)
 
