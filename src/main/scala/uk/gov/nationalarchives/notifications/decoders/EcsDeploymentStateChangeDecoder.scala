7ac0e827ccc8507659599f92b4fd060d14877e88
VimleshGupta <vimlesh.gupta@nationalarchives.gov.uk>
2026-08-24 23:23:47 +0100

    Update event decoder

--- src/main/scala/uk/gov/nationalarchives/notifications/decoders/EcsDeploymentStateChangeDecoder.scala
+++ src/main/scala/uk/gov/nationalarchives/notifications/decoders/EcsDeploymentStateChangeDecoder.scala
@@ -2,18 +2,30 @@ package uk.gov.nationalarchives.notifications.decoders
 
 object EcsDeploymentStateChangeDecoder {
   case class EcsTaskContainer(
-                               name: String,
-                               image: String,
-                               exitCode: Int
+                               name: Option[String],
+                               exitCode: Option[Int],
+                               reason: Option[String]
                              )
 
   case class EcsDeploymentDetail(
-                                  taskArn: String,
-                                  containers: List[EcsTaskContainer]
+                                  clusterArn: Option[String],
+                                  taskArn: Option[String],
+                                  lastStatus: Option[String],
+                                  stopCode: Option[String],
+                                  stoppedReason: Option[String],
+                                  containers: Option[List[EcsTaskContainer]]
                                 )
 
   case class EcsDeploymentStateChangeEvent(
-                                            `detail-type`: String,
-                                            detail: EcsDeploymentDetail
+                                            version: Option[String],
+                                            id: Option[String],
+                                            `detail-type`: Option[String],
+                                            source: Option[String],
+                                            account: Option[String],
+                                            time: Option[String],
+                                            region: Option[String],
+                                            resources: Option[String],
+                                            detail: Option[EcsDeploymentDetail]
                                           ) extends IncomingEvent
 }
+
